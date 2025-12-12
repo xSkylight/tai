cdc3b838712d9b1ed4f08b94e6b64f4a1a10ffe0
xSkylight <129980720+xSkylight@users.noreply.github.com>
2025-12-12 20:44:18 +0300

    Update creator.sql

--- zad4/creator.sql
+++ zad4/creator.sql
@@ -1,31 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 02, 2025 at 08:08 PM
--- Wersja serwera: 10.4.32-MariaDB
--- Wersja PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `creator`
---
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `characters`
---
 
 CREATE TABLE `characters` (
   `id` int(11) NOT NULL,
@@ -37,36 +13,14 @@ CREATE TABLE `characters` (
   `level` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Zrzut danych tabeli `characters`
---
-
 INSERT INTO `characters` (`id`, `nickname`, `class`, `strength`, `intelligence`, `agility`, `level`) VALUES
 (1, 'Warrior123', 'Wojownik', 80, 45, 60, 35),
 (2, 'MagicMike', 'Mag', 35, 90, 50, 42),
 (3, 'ShadowHunter', 'Łucznik', 55, 65, 85, 38);
 
---
--- Indeksy dla zrzutów tabel
---
-
---
--- Indeksy dla tabeli `characters`
---
 ALTER TABLE `characters`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `characters`
---
 ALTER TABLE `characters`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
